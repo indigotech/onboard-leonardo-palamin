07a9b4930400b3a0f1e4035f73adacabd1c2e693
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-05-07 01:40:43 -0300

    feat: added email validator

---
+++ src/utils/email-being-used-validator.ts
@@ -0,0 +1,13 @@
+import { getRepository } from "typeorm";
+import { User } from "../api/user";
+
+export const emailBeingUsedValidate = async (email: string) => {
+  const repository = getRepository(User);
+
+  const emailAlreadyBeingUsed = await repository.findOne({ email });
+  if (emailAlreadyBeingUsed) {
+    throw "Este endereço de email já está sendo usado. Por favor, utilize outro."
+  } else {
+    return true
+  }
+}
