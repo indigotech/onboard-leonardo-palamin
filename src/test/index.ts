ce56479e8897aa62306992656a7d337f3b933758
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-05-12 18:23:37 -0300

    fix: async functions

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,20 +1,19 @@
 import { Database } from "../config/database";
 
-const request = require('supertest');
+const request = require("supertest");
 
-describe('GET /hello', () => {
-  Database.config()
-  it('responds with json', (done) => {
-    const supertest = request('http://localhost:4000');
-    supertest
-      .post('/graphql')
-      .send({ query: '{ helloWorld }' })
-      .set('Accept', 'application/json')
+describe("GET /hello", () => {
+  before(async () => {
+    await Database.config();
+  });
+  it("responds with json", async () => {
+    const supertest = request("http://localhost:4000");
+    await supertest
+      .post("/graphql")
+      .send({ query: "{ helloWorld }" })
+      .set("Accept", "application/json")
       .expect("Content-Type", /json/)
       .expect(200)
-      .end((err: any) => {
-        if (err) console.log(err.message);
-      });
-    done();
   });
 });
+
