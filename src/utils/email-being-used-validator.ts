fc11df8518d360af653cf6891177a88a1b734a82
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-05-13 02:32:54 -0300

    feat: error handling

--- src/utils/email-being-used-validator.ts
+++ src/utils/email-being-used-validator.ts
@@ -1,11 +1,12 @@
 import { getRepository } from "typeorm";
 import { User } from "../api/user";
+import { AuthError } from "./error-handling";
 
 export const validateEmail = async (email: string) => {
   const repository = getRepository(User);
 
   const emailAlreadyBeingUsed = await repository.findOne({ email });
   if (emailAlreadyBeingUsed) {
-    throw "Este endereço de email já está sendo usado. Por favor, utilize outro.";
+    throw new AuthError("Este endereço de email já está sendo usado. Por favor, utilize outro.");
   }
 };
