a4380b3482bc1ab185923ee542cd28a493f99700
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-05-13 16:08:47 -0300

    fix: code review fixes and improvements

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,24 +1,35 @@
 import { createConnection } from "typeorm";
 import { User } from "../api";
+import dotenv from "dotenv";
+
+dotenv.config({ path: process.env.TEST_RUNNING === "TRUE" ? "./.test.env" : "./.env" });
 
 interface DatabaseProps {
-  username: string;
-  password: string;
-  databaseName: string;
-  port: number;
+  username: string | undefined;
+  password: string | undefined;
+  databaseName: string | undefined;
+  port: number | undefined;
 }
 
+const databaseProps: DatabaseProps = {
+  port: process.env.DATABASE_PORT as number | undefined,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  databaseName: process.env.DATABASE_NAME,
+};
+
 export class Database {
-  static async config({ port, username, password, databaseName }: DatabaseProps): Promise<void> {
+  static async config(): Promise<void> {
     await createConnection({
       type: "postgres",
       host: "localhost",
-      port: +port,
-      username: username,
-      password: password,
-      database: databaseName,
+      port: databaseProps.port,
+      username: databaseProps.username,
+      password: databaseProps.password,
+      database: databaseProps.databaseName,
       entities: [User],
       synchronize: true,
     });
+    console.log(process.env.DATABASE_CONNECTED_MESSAGE);
   }
 }
--- src/test/index.ts
+++ src/test/index.ts
@@ -3,68 +3,49 @@ import request from "supertest";
 import crypto from "crypto";
 import { getRepository } from "typeorm";
 import { User } from "../api";
-import { Database } from "../config/database";
-import dotenv from "dotenv";
+import { Server } from "../server";
+import { postGraphQL } from "./post-graphql";
 
-dotenv.config({ path: process.env.TEST_RUNNING === "TRUE" ? "./.test.env" : "./.env" });
+describe("Running tests", () => {
+  //Setting up Server and Database
+  before(async () => {
+    await Server();
+  });
 
-describe("Receive Hello, Onboard!", () => {
-  it("responds with Hello, Onboard!", async () => {
-    const supertest = request("http://localhost:" + process.env.PORT);
-    const res = await supertest.post("/graphql").send({ query: "{ helloWorld }" }).set("Accept", "application/json");
+  //Testing Hello Query
+  it("Query 'hello' responds with Hello, Onboard!", async () => {
+    const helloQuery = "{ helloWorld }"
+    const res = await postGraphQL(helloQuery);
     expect(res.body.data.helloWorld).to.be.eq("Hello, onboard!");
   });
-});
 
-
-const TestDatabaseVars = {
-  port: Number(process.env.DATABASE_PORT),
-  username: String(process.env.DATABASE_USERNAME),
-  password: String(process.env.DATABASE_PASSWORD),
-  databaseName: String(process.env.DATABASE_NAME),
-};
-
-// Mutation data
-const mutation = `
-  mutation($user: UserInput!) {
-    createUser(user: $user) {
-      id
-      name
-      email
-      birthDate
+  //Testing Create User Mutation
+  it("Creates user in database", async () => {
+    const createUserMutation = `
+    mutation($user: UserInput!) {
+      createUser(user: $user) {
+        id
+        name
+        email
+        birthDate
+      }
     }
-  }
-  `;
+    `;
+    const createUserMutationVariables = { user: {
+      name: "Leo",
+      email: "leonardo.palamim@taqtile.com.br",
+      password: "23er23er",
+      birthDate: "31-03-1998",
+    }};
+  
+    const res = await postGraphQL(createUserMutation, createUserMutationVariables);
+    expect(+res.body.data.createUser.id).to.be.above(0);
+    expect(res.body.data.createUser.name).to.be.eq(createUserMutationVariables.user.name);
+    expect(res.body.data.createUser.email).to.be.eq(createUserMutationVariables.user.email);
+    expect(res.body.data.createUser.birthDate).to.be.eq(createUserMutationVariables.user.birthDate);
 
-const createUserName = String("Leo");
-const createUserEmail = String("leonardo.palamim@taqtile.com.br");
-const createUserPassword = String("23er23er");
-const createUserBirthDate = String("31-03-1998");
-
-const variables = {
-  name: createUserName,
-  email: createUserEmail,
-  password: createUserPassword,
-  birthDate: createUserBirthDate,
-};
-
-describe("Create user", () => {
-  before(async () => {
-    await Database.config(TestDatabaseVars);
-  });
-  it("creates user", async () => {
-    const supertest = request("http://localhost:4000");
-    const res = await supertest
-      .post("/graphql")
-      .send({ query: mutation, variables: { user: variables } })
-      .set("Accept", "application/json");
-    expect(Number(res.body.data.createUser.id)).to.be.above(0);
-    expect(res.body.data.createUser.name).to.be.eq(createUserName);
-    expect(res.body.data.createUser.email).to.be.eq(createUserEmail);
-    expect(res.body.data.createUser.birthDate).to.be.eq(createUserBirthDate);
-
-    const encryptedPassword = crypto.createHash("sha256").update(createUserPassword).digest("hex");
-    const createdUser = await getRepository(User).findOne(Number(res.body.data.createUser.id));
+    const encryptedPassword = crypto.createHash("sha256").update(createUserMutationVariables.user.password).digest("hex");
+    const createdUser = await getRepository(User).findOne(+res.body.data.createUser.id);
     expect(createdUser?.password).to.be.eq(encryptedPassword);
   });
 });
