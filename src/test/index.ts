5f757759691e43c8abb0b6826643e33a569e7bb0
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-05-13 01:51:13 -0300

    feat: create user test

--- src/test/index.ts
+++ src/test/index.ts
@@ -60,3 +60,57 @@ describe("Running tests", () => {
     expect(createdUser?.password).to.be.eq(encryptedPassword);
   });
 });
+
+dotenv.config({ path: process.env.TEST_RUNNING === "TRUE" ? "./.test.env" : "./.env" });
+
+const TestDatabaseVars = {
+  port: Number(process.env.DATABASE_PORT),
+  username: String(process.env.DATABASE_USERNAME),
+  password: String(process.env.DATABASE_PASSWORD),
+  databaseName: String(process.env.DATABASE_NAME),
+};
+
+// Mutation data
+const mutation = `
+  mutation($user: UserInput!) {
+    createUser(user: $user) {
+      id
+      name
+      email
+      birthDate
+    }
+  }
+  `;
+
+const createUserName = String("Leo");
+const createUserEmail = String("leonardo.palamim@taqtile.com.br");
+const createUserPassword = String("23er23er");
+const createUserBirthDate = String("31-03-1998");
+
+const variables = {
+  name: createUserName,
+  email: createUserEmail,
+  password: createUserPassword,
+  birthDate: createUserBirthDate,
+};
+
+describe("Create user", () => {
+  before(async () => {
+    await Database.config(TestDatabaseVars);
+  });
+  it("creates user", async () => {
+    const supertest = request("http://localhost:4000");
+    const res = await supertest
+      .post("/graphql")
+      .send({ query: mutation, variables: { user: variables } })
+      .set("Accept", "application/json");
+    expect(Number(res.body.data.createUser.id)).to.be.above(0);
+    expect(res.body.data.createUser.name).to.be.eq(createUserName);
+    expect(res.body.data.createUser.email).to.be.eq(createUserEmail);
+    expect(res.body.data.createUser.birthDate).to.be.eq(createUserBirthDate);
+
+    const encryptedPassword = crypto.createHash("sha256").update(createUserPassword).digest("hex");
+    const createdUser = await getRepository(User).findOne(Number(res.body.data.createUser.id));
+    expect(createdUser?.password).to.be.eq(encryptedPassword);
+  });
+});
