e661cb06c5bf1a1954f7c2e00ac4b91bf89dd725
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-05-14 03:10:41 -0300

    feat: login mutation

--- src/resolver-map.ts
+++ src/resolver-map.ts
@@ -1,10 +1,10 @@
 import { IResolvers } from "graphql-tools";
 import { User } from "./api/user";
-import { UserResponse } from "./schema";
 import { getRepository } from "typeorm";
 import { validatePassword } from "./utils/password-validator";
 import { validateEmail } from "./utils/email-being-used-validator";
 import crypto from "crypto";
+import { UserInput, LoginInput } from "./schema/types";
 
 const resolverMap: IResolvers = {
   Query: {
@@ -13,7 +13,7 @@ const resolverMap: IResolvers = {
     },
   },
   Mutation: {
-    createUser: async (_parent: any, { user: args }: { user: UserResponse }) => {
+    createUser: async (_parent: any, { user: args }: { user: UserInput }) => {
       const user = new User();
       user.name = args.name;
       user.email = args.email;
@@ -30,6 +30,16 @@ const resolverMap: IResolvers = {
 
       const newUser = areCredentialsCorrect ? await getRepository(User).save(user) : null;
       return newUser;
+    },
+    login: async (_parent: any, { data: args }: { data: LoginInput }) => {
+      const user = {
+        id: 10,
+        name: "Leo",
+        email: args.email,
+        birthDate: "31-03-1998"
+      };
+      const token = "the_token";
+      return { user, token }
     },
   },
 };
